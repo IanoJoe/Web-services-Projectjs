e225450de8cf42e6d9aeb061ef13d9aaea82e091
IanoJoe <ianojoe@yahoo.co.uk>
2017-05-07 20:07:40 +0100

    Update index.js

--- api/members/index.js
+++ api/members/index.js
@@ -110,6 +110,20 @@ exports.find = function(req, res) {
 
 };
 
- 
+ var Datastore = require('nedb');
+var db = new Datastore({ filename: __dirname + '/db.json', autoload: true });
+
+db.findOne({ _id: 1 }, function (err, doc) {
+  doc = doc || { _id: 1, counter: 0 };
+
+  console.log('This instance of code was executed ' + doc.counter + ' times. Last access time was ' + doc.lastSeetAt);
+
+  doc.lastSeetAt = new Date();
+  doc.counter++;
+
+  db.update({ _id: 1 }, doc, { upsert: true }, function (err, num) {
+    console.log('Updated ' + num + ' records');
+  });
+});
 
  
